d5ea488986265a70724aebe634e5e932d07cb8ab
NickMercer <dekori217@gmail.com>
2023-03-21 17:29:09 -0700

    Animal configs

---
+++ Assets/SimpleUtilityFramework/Animals/AnimalConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace SimpleUtilityFramework.Animals
+{
+    [CreateAssetMenu(fileName = "Animal Config", menuName = "New Animal Config", order = 0)]
+    public class AnimalConfig : ScriptableObject
+    {
+        [SerializeField]
+        private Sprite _sprite;
+        public Sprite Sprite => _sprite;
+
+        [SerializeField]
+        private string _speciesName;
+        public string SpeciesName => _speciesName;
+    }
+}
